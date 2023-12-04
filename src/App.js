89e95d578e48f5e02916b02d79c1471e31c7812e
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-12-04 16:48:03 -0500

    removed 'unused' code from app file to enable build the project by commenting Firebase import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { FirebaseProvider } from './providers/FirebaseProvider';
+// import { FirebaseProvider } from './providers/FirebaseProvider';
 // import SignUp from './components/SignUp';
 // import Login from './components/Login';
 import Home from './components/Home';

76ef4bdaee795d81040a1b91bcf7a0ebbdea6a99
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-12-06 19:22:40 -0500

    Testing why commits do not appear in github

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,14 +7,9 @@ const Home = ({ projects, notifications }) => {
   const pageTitle = "Home";
   return (
     <Header title={pageTitle} />
-    /*<div>
-      <h1>Home</h1>
-      <p>Number of projects: {projects.length}</p>
-      {// Add similar sections for tasks in 'pending', 'active', 'late', 'completed' }
-      <NavLink to="/project/1">Project 1</NavLink>
-    </div>*/
   );
 };
+
 const Landing = () => {
   const pageTitle = "Home";
   return (
